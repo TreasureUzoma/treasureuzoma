d5c2ee7a0fb693a8b8c8110ebea14d56ae513e8f
TreasureUzoma <123127790+TreasureUzoma@users.noreply.github.com>
2023-02-07 23:52:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function menu(){
+var x = document.getElementById("mi")
+x.style.display = "block"
+var y = document.getElementById("mo")
+y.style.display ="none"
+var z = document.getElementById("mc")
+z.style.display ="block"
+}
+function menuc() {
+
+var x = document.getElementById("mi")
+x.style.display = "none"
+var y = document.getElementById("mo")
+y.style.display ="block"
+var z = document.getElementById("mc")
+z.style.display ="none"
+}
